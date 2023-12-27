e6bdbf146cb42e007393a9655faaeb265ec19e5a
wenjian <114489902+wenjean@users.noreply.github.com>
2023-12-27 16:23:48 +0800

    Add files via upload

---
+++ hand2.py
@@ -0,0 +1,61 @@
+import torch
+from transformers import T5EncoderModel, T5Tokenizer
+import re
+import gc
+import lightgbm
+from Extract_feature import *
+import pickle
+import sys
+from sklearn.metrics import roc_auc_score
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+from ML_grid_search_model import *
+
+def Load_data():
+    print('Data Loading...')
+    Sequence = []
+    with open('/home/wenjian/IPPF-FE-main/bin-dataset/test-970-fasta.txt', 'r') as myfile:
+        for line in myfile:
+            if line[0] != '>':
+                Sequence.append(line.strip('\n'))
+
+    Mysequence = []
+    for i in range(len(Sequence)):
+        zj = ''
+        for j in range(len(Sequence[i]) - 1):
+            zj += Sequence[i][j] + ' '
+        zj += Sequence[i][-1]
+        Mysequence.append(zj)
+    return Sequence, Mysequence
+sequence, sequence1 = Load_data()
+
+strs = sequence
+len_str = len(strs[0])
+min_num_index = 0   # 最小值的下标
+stack = [strs[0]]   # 利用栈来找出最短的字符串
+for index, string in enumerate(strs):
+    if len(string) < len_str:
+        stack.pop()
+        len_str = len(string)
+        min_num_index = index # 知道最短字符对应的下标后，也可以自己找出最短字符
+        stack.append(string)
+print("最短字符串长度:", len_str)
+print("最短字符串下标:", min_num_index)
+print("最短字符串:", stack)
+print("最短字符串:", strs[min_num_index])
+
+
+unique_letters = set()
+
+for sequence in strs:
+    unique_letters.update(sequence)
+
+print(unique_letters)
+
+
+print(sequence[1],len(sequence[1]))
+features_crafted=Get_features(strs,6 )
+print('feature_crafted:',len(features_crafted),len(features_crafted[1]))
+with open('/home/wenjian/IPPF-FE-main/bin-dataset/bin-testx-hand6.pkl', 'wb') as f:
+    pickle.dump(features_crafted, f)
---
+++ t5.py
@@ -0,0 +1,80 @@
+import torch
+from sklearn import metrics
+from transformers import T5EncoderModel, T5Tokenizer
+import re
+import gc
+import lightgbm
+import pickle
+import sys
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import numpy as np
+
+def Load_data():
+    import pandas as pd
+    # 读取CSV文件，假设第一列是序列，第二列是标签
+    data= pd.read_csv('/home/wenjian/IPPF-FE-main/datasetPLM/train.csv', header=None)
+    import numpy as np
+    # （1）获取第1列
+    Sequence = data.iloc[1:,0]
+    Sequence =np.array(Sequence)
+    Mysequence = []
+    for i in range(len(Sequence)):
+        zj = ''
+        for j in range(len(Sequence[i])-1):
+            zj += Sequence[i][j] + ' '
+        zj += Sequence[i][-1]
+        Mysequence.append(zj)
+    label = data.iloc[1:,1]
+    label = np.array(label)
+    return Mysequence, label
+
+def ALL_features(sequences_Example):
+    # Crafted features
+    # Automatic extracted features
+    tokenizer = T5Tokenizer.from_pretrained("prot_t5_xl_uniref50", do_lower_case=False)
+    model = T5EncoderModel.from_pretrained("prot_t5_xl_uniref50")
+    gc.collect()
+    print(torch.cuda.is_available())
+    # 'cuda:0' if torch.cuda.is_available() else
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    model = model.eval()
+    features = []
+    for i in range(len(sequences_Example)):
+        print('For sequence ', str(i+1))
+        sequences_Example_i = sequences_Example[i]
+        sequences_Example_i = [re.sub(r"[UZOB]", "X", sequences_Example_i)]
+        ids = tokenizer.batch_encode_plus(sequences_Example_i, add_special_tokens=True, padding=True)
+        input_ids = torch.tensor(ids['input_ids']).to(device)
+        attention_mask = torch.tensor(ids['attention_mask']).to(device)
+        with torch.no_grad():
+            embedding = model(input_ids=input_ids, attention_mask=attention_mask)
+        embedding = embedding.last_hidden_state.cpu().numpy()
+        
+        for seq_num in range(len(embedding)):
+            seq_len = (attention_mask[seq_num] == 1).sum()
+            seq_emd = embedding[seq_num][:seq_len - 1]
+            features.append(seq_emd)
+    features_normalize = np.zeros([len(features), len(features[0][0])], dtype=float)
+    for i in range(len(features)):
+        for k in range(len(features[0][0])):
+            for j in range(len(features[i])):
+                features_normalize[i][k] += features[i][j][k]
+            features_normalize[i][k] /= len(features[i])
+    print(len(features_normalize), len(features_normalize[0]))
+    return features_normalize
+Mysequence, label_embedding=Load_data()
+
+with open('/mnt/raid5/data4/jwen/NetBCE-mainn/NetBCE-main/data/bcetrain.pkl', 'rb') as f:
+    loaded_sequences = pickle.load(f)
+featuers_embedding_normalize=ALL_features(loaded_sequences)
+featuers_embedding_normalize =np.array(featuers_embedding_normalize)
+print('特征数量与维度：',featuers_embedding_normalize.shape)
+with open('bce-train.pkl', 'wb') as f:
+         pickle.dump(featuers_embedding_normalize, f)
+#print('标签数量：',label_embedding.shape)
+# with open('test-featuers_embedding_normalize.pkl', 'wb') as f:
+#         pickle.dump(featuers_embedding_normalize, f)
+# with open('test-label_embedding.pkl', 'wb') as f:
+#         pickle.dump(label_embedding, f)
